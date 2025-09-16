febf996114c9469d2e0d729cbab2f564c706f8e7
josephwere <josephogwe8@gmail.com>
2025-09-16 14:42:14 +0300

    Update login.js

--- EdgeFi_Windows_Full_Package/EdgeFi_Frontend_Vercel/pages/login.js
+++ EdgeFi_Windows_Full_Package/EdgeFi_Frontend_Vercel/pages/login.js
@@ -1,1 +1,7 @@
-export default function Login(){return <div>Login Page Placeholder</div>}
+const api = process.env.NEXT_PUBLIC_API_URL;
+
+const res = await fetch(${api}/users/login, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ email, password }),
+});
